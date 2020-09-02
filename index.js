bb5c4963bdf43d2409f1c5131f4e275016054fb1
yuuhifuchioka <69574777+yuuhifuchioka@users.noreply.github.com>
2020-09-02 17:00:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/g/', (req, res) => res.json({method: "こんにちは、getさん"})) // 追加
+  .post('/p/', (req, res) => res.json({method: "こんにちは、postさん"})) // 追加
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
