9dbbca53451a7c368a184de4be59fa145cac813e
yuuhifuchioka <69574777+yuuhifuchioka@users.noreply.github.com>
2020-09-02 17:07:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/g/', (req, res) => res.json({method: "こんにちは、getさん"})) // 追加
-  .post('/p/', (req, res) => res.json({method: "こんにちは、postさん"})) // 追加
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/g/', (req, res) => res.json({method: "こんにちは、getさん"})); // 追加
+app.post('/p/', (req, res) => res.json({method: "こんにちは、postさん"})); // 追加
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
