d8c6f95979182bdb6fe22b7d5f83caa261eb2ab8
yuuhifuchioka <69574777+yuuhifuchioka@users.noreply.github.com>
2020-09-02 17:24:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/g/', (req, res) => {
           const getStr = "Hello Get.";
           const getJson = "{method:" + getStr + "}";
           res.json({method: "Hello Get."});
+          }
        ); // 追加
 app.post('/p/', (req, res) => res.json({method: "こんにちは、postさん"})); // 追加
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
